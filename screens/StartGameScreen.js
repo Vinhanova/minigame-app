22c0069618497c7904cb65bba6659288eb9ca2a2
Vinhanova <tiagovinhanova@gmail.com>
2023-11-13 22:10:09 +0000

    Layout & Style improvements

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,24 +5,38 @@ const StartGameScreen = () => {
   return (
     <View style={styles.inputContainer}>
       <TextInput style={styles.numberInput} maxLength={2} keyboardType='number-pad' autoCapitalize='none' autoCorrect={false} />
-      <PrimaryButton>Confirm</PrimaryButton>
-      <PrimaryButton>Reset</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
     padding: 16,
     marginTop: 100,
     marginHorizontal: 24,
     borderRadius: 8,
-    backgroundColor: '#4e0329',
+    backgroundColor: '#3b021f',
     elevation: 4, //android
     shadowColor: 'black', //ios
     shadowOffset: { width: 0, height: 2 }, //ios
     shadowRadius: 6, //ios
     shadowOpacity: 0.25 //ios
+  },
+  buttonsContainer: {
+    flexDirection: 'row'
+  },
+  buttonContainer: {
+    flex: 1
   },
   numberInput: {
     height: 50,
